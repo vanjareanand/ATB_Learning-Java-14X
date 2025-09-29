65e3e23c31fd4281bccaf385a2684d4e30610422
vanjareanand <vanjare.anand21@gmail.com>
2025-09-30 02:19:47 +0530

    Test Class one

---
+++ src/Ex_Lab_001_Java_Basic/Lab_001_Java_Basic.java
@@ -0,0 +1,13 @@
+package Ex_Lab_001_Java_Basic;
+
+public class Lab_001_Java_Basic {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+
+    }
+}
