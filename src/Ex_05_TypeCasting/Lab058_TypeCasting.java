46315de03e58d36e908ab21458ad1bfa59caed84
vanjareanand <vanjare.anand21@gmail.com>
2025-10-01 13:37:16 +0530

    TypeCasting

---
+++ src/Ex_05_TypeCasting/Lab058_TypeCasting.java
@@ -0,0 +1,10 @@
+package Ex_05_TypeCasting;
+
+public class Lab058_TypeCasting {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b; // Valid Syntax -> Widening -> Implicit Casting - Automatically done.
+        int a1 = (int) b; // Valid Syntax -> Widening -> Explicit Casting - not requred
+    }
+}
---
+++ src/Ex_05_TypeCasting/Lab060_TypeCasting.java
@@ -0,0 +1,12 @@
+package Ex_05_TypeCasting;
+
+public class Lab060_TypeCasting {
+
+    public static void main(String[] args) {
+        long phone_no = 9876543210l;
+//        short s = phone_no; // Narrowing - implicit
+        short s = (short) phone_no; // Narrowing - Explicit
+        System.out.println(s);
+
+    }
+}
